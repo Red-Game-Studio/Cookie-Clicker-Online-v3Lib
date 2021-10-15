9d6bc03e95b80699c20c5aea60c1cdbdda7e0666
Red <72632024+RedBigz@users.noreply.github.com>
2021-10-16 10:01:50 +1100

    Update client.js

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ class OnlineClientLoader_Client {
   }
 
   start() {
-    this.v3.join(prompt("Server Code", "AAAA"), prompt("Username", "Guest"));
+    alert(1);
+    //this.v3.join(prompt("Server Code", "AAAA"), prompt("Username", "Guest"));
   }
 }
